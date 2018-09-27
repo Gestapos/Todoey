7d8aab93e622028177ce9e1ecd0c935b7d06cd19
Alexandr <Sashob84@gmail.com>
2018-09-27 17:33:02 +0300

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alexandr on 9/27/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
